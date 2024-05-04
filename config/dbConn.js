d6638583a3149df2aa752b92c073b57934e1eac9
Nguyen-Gia-Bao <giabao20924@gmail.com>
2024-05-04 23:52:25 +0700

    cc

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb';
 
 // Connecting to MongoDB
 export async function createConnection() {
-  const MONGO_URL = "mongodb://localhost:27017/Dashboard/";
+  const MONGO_URL = "mongodb://localhost:27017/Dashboard";
   const client = new MongoClient(MONGO_URL);
   try {
     await client.connect();
